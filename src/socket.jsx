c836dcc6520435636e1b1b50c13422ddd8e1ad85
kuljeet singh <kuljeet9186@gmail.com>
2024-06-19 22:50:36 +0530

    Update socket.jsx

--- src/socket.jsx
+++ src/socket.jsx
@@ -7,7 +7,7 @@ const SocketContext = createContext();
 const getSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io("http://localhost:3000", { withCredentials: true }), []);
+  const socket = useMemo(() => io(server, { withCredentials: true }), []);
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
